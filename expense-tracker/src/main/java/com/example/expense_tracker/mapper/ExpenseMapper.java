9515334086597da4d4907464b8402cbcc566ebba
Bilguun Batsaikhan <bilguun.batsaikhan@bitspa.it>
2026-01-14 10:34:48 +0100

    Added mapstruct for expense

--- expense-tracker/src/main/java/com/example/expense_tracker/mapper/ExpenseMapper.java
+++ expense-tracker/src/main/java/com/example/expense_tracker/mapper/ExpenseMapper.java
@@ -1,33 +1,17 @@
 package com.example.expense_tracker.mapper;
 
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import com.example.expense_tracker.dto.expense.ExpenseRequestDto;
 import com.example.expense_tracker.dto.expense.ExpenseResponseDto;
 import com.example.expense_tracker.entities.Expense;
 
-@Component
-public class ExpenseMapper {
-    public ExpenseResponseDto toDto(Expense expense) {
-        ExpenseResponseDto expenseResponseDto = new ExpenseResponseDto();
-        expenseResponseDto.setId(expense.getId());
-        expenseResponseDto.setAmount(expense.getAmount());
-        expenseResponseDto.setCurrency(expense.getCurrency());
-        expenseResponseDto.setDescription(expense.getDescription());
-        expenseResponseDto.setExpenseDate(expense.getExpenseDate());
-        expenseResponseDto.setCategoryId(expense.getCategory().getId());
-        expenseResponseDto.setCategoryName(expense.getCategory().getName());
-        expenseResponseDto.setCreatedAt(expense.getCreatedAt());
-        return expenseResponseDto;
-    }
+@Mapper(componentModel = "spring")
+public interface ExpenseMapper {
+    @Mapping(source = "category.id", target = "categoryId")
+    @Mapping(source = "category.name", target = "categoryName")
+    public ExpenseResponseDto toDto(Expense expense);
 
-    public Expense toEntity(ExpenseRequestDto expenseRequestDto) {
-        Expense expense = new Expense();
-        expense.setAmount(expenseRequestDto.getAmount());
-        expense.setDescription(expenseRequestDto.getDescription());
-        expense.setExpenseDate(expenseRequestDto.getExpenseDate());
-        expense.setCurrency(expenseRequestDto.getCurrency());
-        expense.setDeleted(false);
-        return expense;
-    }
+    public Expense toEntity(ExpenseRequestDto expenseRequestDto);
 }
